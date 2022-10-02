caa5c9d3a872a879ec885f89de7e77737b7976cb
Jordan Kniest <mail@jkniest.de>
2022-10-02 22:34:56 +0200

    Allow to switch between recipes

---
+++ Assets/Scripts/Ui/Data/MealRecipeUi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Ui.Data
+{
+    [Serializable]
+    public class MealRecipeUi
+    {
+        public GameObject blockerElement;
+        public GameObject recipeListElement;
+    }
+}
---
+++ Assets/Scripts/Ui/RecipePreview.cs
@@ -0,0 +1,53 @@
+﻿using Ui.Data;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Ui
+{
+    public class RecipePreview : MonoBehaviour
+    {
+        public MealRecipeUi[] recipes;
+
+        private int _active = 0;
+
+        public void OnNextRecipe(InputAction.CallbackContext context)
+        {
+            if (!context.performed)
+            {
+                return;
+            }
+            
+            recipes[_active].blockerElement.SetActive(false);
+            recipes[_active].recipeListElement.SetActive(false);
+
+            _active++;
+            if (_active >= recipes.Length)
+            {
+                _active = 0;
+            }
+            
+            recipes[_active].blockerElement.SetActive(true);
+            recipes[_active].recipeListElement.SetActive(true);
+        }
+
+        public void OnPreviousRecipe(InputAction.CallbackContext context)
+        {
+            if (!context.performed)
+            {
+                return;
+            }
+            
+            recipes[_active].blockerElement.SetActive(false);
+            recipes[_active].recipeListElement.SetActive(false);
+
+            _active--;
+            if (_active < 0)
+            {
+                _active = recipes.Length - 1;
+            }
+            
+            recipes[_active].blockerElement.SetActive(true);
+            recipes[_active].recipeListElement.SetActive(true);
+        }
+    }
+}
